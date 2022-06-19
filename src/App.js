d936ae05c810890daf8d102ef6adbb3c7dc6fe31
陳浩偉 <chenhaowei@chenhaoweideMacBook-Pro.local>
2022-06-19 21:31:54 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes } from 'react-router-dom'; 
+// 舊版本是Switch+Route
+
+import AllMeetupsPage from './pages/AllMeetups';
+import NewMeetupPage from './pages/NewMeetup';
+import FavoritePage from './pages/Favorites';
+import MainNavigation from './components/layout/MainNavigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MainNavigation />
+      <Routes>
+        <Route path='/' element={<AllMeetupsPage />} />
+        <Route path='/new-meetup' element={<NewMeetupPage />} />
+        <Route path='/favorites' element={<FavoritePage />} />
+
+        {/* 原本老師是以下方的方式進行路由的配置但因為老師的dom版本為5. 當前最新為6. 因此改以上方element取代舊的配置 */}
+        {/* <Route path='/favorites'>
+          <FavoritePage />
+        </Route> */}
+      </Routes>
     </div>
   );
 }
